dde65e43fb972b3bb2fd16ef61da452afd081d52
OccultArcanne <127837066+OccultArcanne@users.noreply.github.com>
2023-04-07 14:13:26 -0600

    Update voidpkgs.sh

--- scripts/voidpkgs.sh
+++ scripts/voidpkgs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Instalar paquetes necesarios
-sudo xbps-install -S void-repo-nonfree util-linux bsdtar coreutils binutils libatomic-devel base-devel curl wget linux-firmware-amd dbus polkitd NetworkManager xtools git bat ripgrep fd exa procs helix neovim htop neofetch ntfs-3g p7zip dust fish-shell starship &&
+sudo xbps-install -S void-repo-nonfree util-linux bsdtar coreutils binutils libatomic-devel base-devel curl wget linux-firmware-amd dbus polkitd NetworkManager xtools git bat ripgrep fd exa procs helix neovim htop neofetch ntfs-3g p7zip dust fish-shell starship neovim lazygit &&
 sudo xbps-install -Su &&
